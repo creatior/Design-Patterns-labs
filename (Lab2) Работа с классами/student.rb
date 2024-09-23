844f8f2c09cf6372400f6d4af57a7d801858f3fb
creatior <vartem2020@mail.ru>
2024-09-23 19:32:39 +0300

    Задача 1.8
    Добавлены методы проверки заполнения всех полей класса

--- (Lab2) Работа с классами/student.rb
+++ (Lab2) Работа с классами/student.rb
@@ -43,19 +43,115 @@ class Student
 		result
 	end
 	
-	# Метод проверки валидности номера телефона
+	# Методы проверки валидности введенных полей
+	# Проверка номера телефона
 	def self.phone_number_valid?(phone_number)
-		if /\A\d{11}\z/.match?(phone_number) || phone_number == nil
+		if /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.match?(phone_number) || phone_number.nil?
 			true
 		else
 			raise ArgumentError, "Неправильная запись номера телефона: #{phone_number}"
 		end
 	end
 	
-	# Переопределение сеттера для phone_number с проверкой
+	# Проверка имени, фамилии и отчества
+	def self.name_valid?(name)
+		if /^[A-ZА-Я][a-zA-Zа-яА-Я\-]{0,49}$/.match?(name)
+			true
+		else 
+			raise ArgumentError, "Неправильная запись ФИО: #{name}"
+		end
+	end
+	
+	# Проверка ID
+	def self.id_valid?(id)
+		if id.nil? || id.class == Integer
+			true
+		else 
+			raise ArgumentError, "Неправильный ввод ID: #{id}"
+		end
+	end
+	
+	# Проверка telegram
+	def self.telegram_valid?(telegram)
+		if /^@[a-zA-Z0-9_]{4,20}$/.match?(telegram) || telegram.nil?
+			true
+		else 
+			raise ArgumentError, "Неправильный ввод telegram: #{telegram}"
+		end
+	end	
+	
+	# Проверка email
+	def self.email_valid?(email)
+		if /^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$/.match?(email) || email.nil?
+			true
+		else 
+			raise ArgumentError, "Неправильный ввод email: #{email}"
+		end
+	end
+	
+	# Проверка git
+	def self.git_valid?(git)
+		if /^(?:https:\/\/github\.com\/)?[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/.match?(git) || git.nil?
+			true
+		else 
+			raise ArgumentError, "Неправильный ввод git: #{git}"
+		end
+	end
+	
+	# Переопределение сеттеров с проверкой полей
+	# Проверка имени
+	def first_name=(first_name)
+		if first_name && Student.name_valid?(first_name)
+			@first_name = first_name
+		end
+	end
+		
+	# Проверка фамилии
+	def surname=(surname)
+		if surname && Student.name_valid?(surname)
+			@surname = surname
+		end
+	end
+	
+	# Проверка отчества
+	def last_name=(last_name)
+		if last_name && Student.name_valid?(last_name)
+			@last_name = last_name
+		end
+	end
+	
+	# Проверка номера телефона
 	def phone_number=(phone_number)
 		if phone_number && Student.phone_number_valid?(phone_number)
 			@phone_number = phone_number
+		end
+	end
+	
+	# Проверка ID
+	def id=(id)
+		if id && Student.id_valid?(id)
+			@id = id
+		end
+	end
+	
+	# Проверка telegram
+	def telegram=(telegram)
+		if telegram && Student.telegram_valid?(telegram)
+			@telegram = telegram
+		end
+	end
+
+	# Проверка email
+	def email=(email)
+		if email && Student.email_valid?(email)
+			@email = email
+		end
+	end
+
+	# Проверка git
+	def git=(git)
+		if git && Student.git_valid?(id)
+			@git = git
 		end
 	end
 end
