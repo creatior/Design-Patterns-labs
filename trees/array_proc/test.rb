5d73fa0cc351f234bc246030520e07f74510ee35
Artyom <82323030+creatior@users.noreply.github.com>
2024-11-16 08:13:32 +0300

    Delete trees/array_proc/test.rb

--- trees/array_proc/test.rb
+++
@@ -1,5 +0,0 @@
-require "./array_proc.rb"
-#array = Array_proc.new([12, 43, 23, 12, 76, 89, 54, 34])
-
-array = Array_proc.new([13, -14, 15, 16, -17, 18, -19, 20])
-puts array.reduce {|acc, num| acc + num}
