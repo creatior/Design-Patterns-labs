9fde86db6b6c69302a6bd7c756a1d0df3359e2a6
creatior <vartem2020@mail.ru>
2024-11-27 22:02:45 +0300

    LR4T3.9 Added class DB_client

--- student/students_list_db.rb
+++ student/students_list_db.rb
@@ -1,16 +1,16 @@
 require_relative "./model/data_list_student_short.rb"
 require_relative "./student.rb"
-require "pg"
+require_relative "./db_client.rb"
 
 class Students_list_DB
-  
+  # constructor
   def initialize(db_config)
-    self.connection = PG.connect(db_config)
+    self.connection = DB_client.new(db_config)
   end
 
   # get student by id
   def get_by_id(id)
-    result = self.connection.exec("SELECT * FROM student WHERE id = #{id}")
+    result = self.connection.query("SELECT * FROM student WHERE id = #{id}")
     result = result.first
 
     return nil unless result
@@ -20,7 +20,7 @@ class Students_list_DB
   # get k n student_short list
   def get_k_n_student_short_list(k, n)
     start_index = (k - 1) * n
-    result = self.connection.exec("SELECT * FROM student LIMIT #{n} OFFSET #{start_index}").to_a
+    result = self.connection.query("SELECT * FROM student LIMIT #{n} OFFSET #{start_index}").to_a
     students_short = (result || []).map { |hash| Student_short.from_student(student:Student.new_from_hash(hash)) }
     data_list ||= Data_list_student_short.new(students_short)
     data_list
@@ -41,7 +41,7 @@ class Students_list_DB
       INSERT INTO student (surname, first_name, last_name, telegram, email, phone_number, git, birthdate)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     SQL
-    self.connection.exec_params(query, [surname, first_name, last_name, telegram, email, phone_number, git, birthdate])
+    self.connection.query(query, [surname, first_name, last_name, telegram, email, phone_number, git, birthdate])
   end
 
   # replace student by id
@@ -60,16 +60,17 @@ class Students_list_DB
       SET surname = $1, first_name = $2, last_name = $3, telegram = $4, email = $5, phone_number = $6, git = $7, birthdate = $8
       WHERE id = $9
     SQL
-    self.connection.exec_params(query, [surname, first_name, last_name, telegram, email, phone_number, git, birthdate, id])
+    self.connection.query(query, [surname, first_name, last_name, telegram, email, phone_number, git, birthdate, id])
   end
 
   # delete by id
   def delete_by_id(id)
-    self.connection.exec("DELETE FROM student WHERE id = #{id}")
+    self.connection.query("DELETE FROM student WHERE id = #{id}")
   end
 
+  # get count of student_short
   def get_student_short_count
-    result = self.connection.exec("SELECT COUNT(*) FROM student")
+    result = self.connection.query("SELECT COUNT(*) FROM student")
     result.first["count"]
   end
 
